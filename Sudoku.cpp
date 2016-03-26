9e2933c1f4c41e15939818cf675b51564dc03209
Randy1005 <randychang.10@gmail.com>
2016-03-26 16:03:41 +0800

    changeRow function finished

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -46,7 +46,109 @@ void Sudoku::changeNum(int a,int b)
 
 void Sudoku::changeRow(int a,int b)
 {
+	int tmp0[9],tmp1[9],tmp2[9];
+	while(a!=b)
+	{
+		if(a == 0){
+			if(b == 1)
+			{
+				for(int i=0;i<9;i++)
+				{
+					tmp0[i] = board[0][i];
+					tmp1[i] = board[1][i];
+					tmp2[i] = board[2][i];
+					board[0][i] = board[3][i];
+					board[1][i] = board[4][i];
+					board[2][i] = board[5][i];
+					board[3][i] = tmp0[i];
+					board[4][i] = tmp1[i];
+					board[5][i] = tmp2[i];		
+				}
+			}
+			else if(b == 2)
+			{
+				for(int i=0;i<9;i++)
+				{
+					tmp0[i] = board[0][i];
+					tmp1[i] = board[1][i];
+					tmp2[i] = board[2][i];
+					board[0][i] = board[6][i];
+					board[1][i] = board[7][i];
+					board[2][i] = board[8][i];
+					board[6][i] = tmp0[i];
+					board[7][i] = tmp1[i];
+					board[8][i] = tmp2[i];		
+				}
+			}
+		}
+		if(a == 1){	
+			if(b == 0)
+			{
+				for(int i=0;i<9;i++)
+				{
+					tmp0[i] = board[0][i];
+					tmp1[i] = board[1][i];
+					tmp2[i] = board[2][i];
+					board[0][i] = board[3][i];
+					board[1][i] = board[4][i];
+					board[2][i] = board[5][i];
+					board[3][i] = tmp0[i];
+					board[4][i] = tmp1[i];
+					board[5][i] = tmp2[i];		
+				}
+			}
+			else if(b == 2)
+			{	
+				for(int i=0;i<9;i++)
+				{
+					tmp0[i] = board[3][i];
+					tmp1[i] = board[4][i];
+					tmp2[i] = board[5][i];
+					board[3][i] = board[6][i];
+					board[4][i] = board[7][i];
+					board[5][i] = board[8][i];
+					board[6][i] = tmp0[i];
+					board[7][i] = tmp1[i];
+					board[8][i] = tmp2[i];		
+				}
+			}
+		}
+		if(a == 2)
+		{
+			if(b == 0)
+			{
+				for(int i=0;i<9;i++)
+				{
+					tmp0[i] = board[0][i];
+					tmp1[i] = board[1][i];
+					tmp2[i] = board[2][i];
+					board[0][i] = board[6][i];
+					board[1][i] = board[7][i];
+					board[2][i] = board[8][i];
+					board[6][i] = tmp0[i];
+					board[7][i] = tmp1[i];
+					board[8][i] = tmp2[i];		
+				}
+			}
+			else if(b == 1)
+			{	
+				for(int i=0;i<9;i++)
+				{
+					tmp0[i] = board[3][i];
+					tmp1[i] = board[4][i];
+					tmp2[i] = board[5][i];
+					board[3][i] = board[6][i];
+					board[4][i] = board[7][i];
+					board[5][i] = board[8][i];
+					board[6][i] = tmp0[i];
+					board[7][i] = tmp1[i];
+					board[8][i] = tmp2[i];		
+				}
+			}
 
+		}
+		break;
+	}
 }
 
 void Sudoku::changeCol(int a,int b)
