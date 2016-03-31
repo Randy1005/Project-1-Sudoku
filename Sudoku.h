148b7d60575ed42074962b160abb7e50a88f1867
Randy1005 <randychang.10@gmail.com>
2016-03-31 15:31:00 +0800

    modified deleted nonnecessary prototypes

--- Sudoku.h
+++ Sudoku.h
@@ -4,7 +4,6 @@
 class Sudoku
 {
 	public:
-		Sudoku();
 		void giveQuestion();
 		void readIn();
 		void solve();
@@ -12,12 +11,27 @@ class Sudoku
 		void changeRow(int a,int b);
 		void changeCol(int a,int b);
 		void rotate(int n);
-		void rotate_multi(int n);
 		void change();
 		void flip(int n);
 		void transform();
-		void printOut(bool isAns);
+		void printOut();
+		/*******************/
+		void push(int sp);
+		int pop();
+		unsigned char numberChkConflict(int sp);
+		int getNextBlankForward(int sp);
+		int getNextBlankBackward(int sp);
+		void Try(int dir);
+		unsigned char preChkNG(int board[][9],int m,int n);
+		int tempNum[81];
+		int tempSp;
+		union UnionSudoku{
+			int sdkuArr1[81];
+			int sdkuArr2[9][9];
+		}sudo;
+		/*****************/
 		bool Try(int board[SIZE][SIZE]);
+		bool Try_backward(int chk_board[SIZE][SIZE]);
 		bool findUnassigned(int board[SIZE][SIZE],int &row,int &col);
 		bool usedInRow(int board[SIZE][SIZE],int row,int num);
 		bool usedInCol(int board[SIZE][SIZE],int col,int num);
@@ -27,7 +41,7 @@ class Sudoku
 		int board[SIZE][SIZE];
 		int rotate_board[SIZE][SIZE];
 		int flip_board[SIZE][SIZE];
-		int chk_multi[SIZE][SIZE];
+		int chk_board[SIZE][SIZE];
 };
 
 
