2e63e0d002293baf63edcb6c3df70bdd1de95e30
Randy1005 <randychang.10@gmail.com>
2016-04-01 11:40:46 +0800

    prototypes modified

--- Sudoku.h
+++ Sudoku.h
@@ -15,21 +15,6 @@ class Sudoku
 		void flip(int n);
 		void transform();
 		void printOut(int grid[SIZE][SIZE]);
-		/*******************/
-		void push(int sp);
-		int pop();
-		unsigned char numberChkConflict(int sp);
-		int getNextBlankForward(int sp);
-		int getNextBlankBackward(int sp);
-		void Try(int dir);
-		unsigned char preChkNG(int board[][9],int m,int n);
-		int tempNum[81];
-		int tempSp;
-		union UnionSudoku{
-			int sdkuArr1[81];
-			int sdkuArr2[9][9];
-		}sudo;
-		/*****************/
 		bool Try(int board[SIZE][SIZE]);
 		bool Try_backward(int chk_board[SIZE][SIZE]);
 		bool findUnassigned(int board[SIZE][SIZE],int &row,int &col);
