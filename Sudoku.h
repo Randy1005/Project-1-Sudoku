410c308855cedb7b316f691d8631d986e23a73f6
Randy1005 <randychang.10@gmail.com>
2016-04-02 16:14:22 +0800

    prototypes modified

--- Sudoku.h
+++ Sudoku.h
@@ -22,11 +22,18 @@ class Sudoku
 		bool usedInCol(int board[SIZE][SIZE],int col,int num);
 		bool usedInCell(int board[SIZE][SIZE],int cellStartRow,int cellStartCol,int num);
 		bool isLegal(int board[SIZE][SIZE],int row,int col,int num);
+		bool preChkRow(int board[SIZE][SIZE]);
+		bool preChkCol(int board[SIZE][SIZE]);
+		bool preChkCell(int board[SIZE][SIZE]);
+		bool isSolvable(int board[SIZE][SIZE]);
 
 		int board[SIZE][SIZE];
 		int rotate_board[SIZE][SIZE];
 		int flip_board[SIZE][SIZE];
 		int chk_board[SIZE][SIZE];
+		int rowFlag[9];
+		int colFlag[9];
+		int cellFlag[9];
 };
 
 
